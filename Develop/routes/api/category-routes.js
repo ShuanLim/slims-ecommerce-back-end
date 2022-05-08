82823f52dbf11b0cc2f304b7b3ba26ea651ef623
s.limguy <shuanmlim@icloud.com>
2022-05-07 21:45:38 -0500

    post route, tested ok

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -40,13 +40,18 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  
-
+  Category.create({
+    category_name: req.body.category_name
+  }).then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-
+  
 });
 
 router.delete('/:id', (req, res) => {
