fa18e1ee1335c1568341c6532d8499f52dd270e4
s.limguy <shuanmlim@icloud.com>
2022-05-07 21:36:43 -0500

    get id route, tested ok

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -23,18 +23,35 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['category_id']
+    }
+  }).then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
 });
 
 module.exports = router;
